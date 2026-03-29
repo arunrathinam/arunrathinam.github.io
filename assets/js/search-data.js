6f68728b491ff2ecc090ca9c15730befbf616496
akum04 <34163713+akum04@users.noreply.github.com>
2026-03-29 18:43:05 +0000

    Deploying to gh-pages from @ arunrathinam/arunrathinam.github.io@a01bac88fc519c26aba523936274cb9947b49314 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,21 +511,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV_2026_academic.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%61%72%75%6E%6B%75%6D%61%72.%72%61%74%68%69%6E%61%6D@%75%6E%69.%6C%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,14 +532,21 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=zC2Ri2MAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/arunsz", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-2231-0734", "_blank");
         },
       },{
       id: 'light-theme',
